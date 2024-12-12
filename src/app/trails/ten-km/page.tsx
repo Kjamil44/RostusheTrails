cdd87870b19ac865160a21b8629d0842388bdec8
Kjamil Kakaleski <kakaleskikjamil@gmail.com>
2024-12-12 01:08:21 +0100

    First commit

---
+++ src/app/trails/ten-km/page.tsx
@@ -0,0 +1,5 @@
+function generateStaticParams() {}
+ 
+export default function Page() {
+  return <h1>Hello, Ten Km Page!</h1>
+}
---
+++ src/app/trails/twentyfour-km/page.tsx
@@ -0,0 +1,5 @@
+function generateStaticParams() {}
+ 
+export default function Page() {
+  return <h1>Hello, Twenty-four Km Page!</h1>
+}
